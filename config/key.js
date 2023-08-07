714f91ecb117c02f53244dec3781af1834c5e915
BannelaRohith <85173640+BannelaRohith@users.noreply.github.com>
2023-08-07 23:13:38 +0530

    Update key.js

--- config/key.js
+++ config/key.js
@@ -1,4 +1,3 @@
 module.exports = {
-    MongoURI:"mongodb+srv://sayanimanna0:XbsraczaoMSMzbBf@cluster0.loebm3g.mongodb.net/myFirstDatabase"
-    // "mongodb+srv://kumar:awxhnpt6Mf9keSRI@cluster0.zsmd20g.mongodb.net/?retryWrites=true&w=majority"
+    MongoURI: 'mongodb://127.0.0.1/Authentication'    
 }
